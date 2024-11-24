f2ef1cd6d1c6918b907d43f648157a569969376c
Josua <josua312@gmail.com>
2024-11-24 15:29:33 +0700

    Inventory DDL

--- tablePostgres.sql
+++ tablePostgres.sql
@@ -15,6 +15,7 @@ CREATE TABLE Pengguna (
     idPengguna SERIAL PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     noHp VARCHAR(30),
+    nama VARCHAR(255) NOT NULL,
     alamat VARCHAR(255), 
     email VARCHAR(255) UNIQUE,
     role rolePengguna NOT NULL DEFAULT 'pengguna',
@@ -30,22 +31,15 @@ CREATE TABLE Transaksi (
     idPengguna INT REFERENCES pengguna(idPengguna) NOT NULL
 );
 
-CREATE TABLE Jenis_Sampah (
-    idJenisSampah SERIAL PRIMARY KEY,
-    namaJenisSampah VARCHAR(20) NOT NULL UNIQUE,
-    isActive BOOLEAN DEFAULT TRUE NOT NULL
-);
-
 CREATE TABLE SatuanKuantitas (
     idSatuanKuantitas SERIAL PRIMARY KEY,
-    naSatuanKuantitas VARCHAR(10) NOT NULL UNIQUE,
+    namaSatuanKuantitas VARCHAR(10) NOT NULL UNIQUE,
     isActive BOOLEAN DEFAULT TRUE NOT NULL
 );
 
 CREATE TABLE Sampah (
     idSampah SERIAL PRIMARY KEY,
     namaSampah VARCHAR(40) NOT NULL UNIQUE,
-    idJenisSampah INT NOT NULL REFERENCES Jenis_Sampah(idJenisSampah),
     urlGambar VARCHAR(255),
     idSatuanKuantitas INT REFERENCES SatuanKuantitas(idSatuanKuantitas),
     isActive BOOLEAN DEFAULT TRUE NOT NULL
@@ -62,13 +56,63 @@ CREATE TABLE Harga (
 ALTER TABLE Sampah
 ADD COLUMN idHargaSekarang INT REFERENCES Harga(idHarga);
 
-CREATE TABLE Transaksi_Sampah (
-    idTransaksi INT REFERENCES Transaksi(idTransaksi),
+CREATE TABLE Inventory_Sampah(
+    idInventorySampah SERIAL PRIMARY KEY,
+    idSampah INT NOT NULL REFERENCES Sampah(idSampah) UNIQUE,
+    kuantitas INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE Transaksi_Masuk (
+    idTransaksiMasuk SERIAL PRIMARY KEY,
+    tanggal TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    idPengguna INT REFERENCES pengguna(idPengguna) NOT NULL
+);
+
+CREATE TABLE Transaksi_Keluar(
+    idTransaksiKeluar SERIAL PRIMARY KEY,
+    tanggal TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE Transaksi_Keluar_Sampah(
+    idTransaksiKeluar INT REFERENCES Transaksi_Keluar(idTransaksiKeluar),
+    idSampah INT NOT NULL REFERENCES Sampah(idSampah),
+    jumlahSampah INT NOT NULL,
+    idHarga INT REFERENCES Harga(idHarga) NOT NULL,
+    PRIMARY KEY (idTransaksiKeluar, idSampah)
+);
+
+
+CREATE TABLE Transaksi_Masuk_Sampah (
+    idTransaksiMasuk INT REFERENCES Transaksi_Masuk(idTransaksiMasuk),
     idSampah INT REFERENCES Sampah(idSampah) NOT NULL,
     idHarga INT REFERENCES Harga(idHarga) NOT NULL,
     jumlahSampah INT NOT NULL,
-    PRIMARY KEY (idTransaksi, idSampah)
+    PRIMARY KEY (idTransaksiMasuk, idSampah)
 );
+
+CREATE OR REPLACE FUNCTION check_jumlahSampah()
+RETURNS TRIGGER AS $$
+BEGIN
+    PERFORM 1 
+    FROM Inventory_Sampah 
+    WHERE idSampah = NEW.idSampah AND kuantitas >= NEW.jumlahSampah;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Insufficient inventory for idSampah: %, requested: %, available: %', 
+                        NEW.idSampah, NEW.jumlahSampah, 
+                        (SELECT kuantitas FROM Inventory_Sampah WHERE idSampah = NEW.idSampah);
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_insert_transaksi_keluar_sampah
+BEFORE INSERT ON Transaksi_Keluar_Sampah
+FOR EACH ROW
+EXECUTE FUNCTION check_jumlahSampah();
+
+
 
 CREATE VIEW get_gambar_nama_harga_satuan AS
 SELECT
@@ -82,6 +126,3 @@ JOIN
     Harga h ON s.idSampah = h.idSampah
 JOIN
     SatuanKuantitas sq ON s.idSatuanKuantitas = sq.idSatuanKuantitas;
-
-ALTER TABLE Pengguna
-ADD COLUMN nama VARCHAR(255);
