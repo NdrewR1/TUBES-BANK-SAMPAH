6b8c7df25808f99bbe86b0936c2c8a72f4777f5e
Josua <josua312@gmail.com>
2024-11-24 11:59:36 +0700

    Add cleanContext in logout

--- src/main/java/com/example/BankSampah/controller/indexController.java
+++ src/main/java/com/example/BankSampah/controller/indexController.java
@@ -1,12 +1,17 @@
 package com.example.BankSampah.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.BankSampah.model.sampah.Sampah;
 import com.example.BankSampah.model.sampah.SampahRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class indexController {
@@ -27,7 +32,13 @@ public class indexController {
     }
 
     @GetMapping("/logoutPage")
-    public String showLogout() {
+    public String showLogout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextHolder.clearContext(); // Clear the security context
+        request.getSession().invalidate(); // Invalidate the session
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute("SPRING_SECURITY_CONTEXT");
+        }
         return "logout";
     }
 }
