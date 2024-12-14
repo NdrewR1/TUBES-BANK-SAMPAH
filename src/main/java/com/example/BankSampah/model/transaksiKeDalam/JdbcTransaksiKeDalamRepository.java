c4a233054eec77fd0dd7d1f0f8fe52a3d98d1a03
unknown <andrewaruan@gmail.com>
2024-12-14 18:40:37 +0700

    Update jdbcTransaksiKeDalam untuk laporan

--- src/main/java/com/example/BankSampah/model/transaksiKeDalam/JdbcTransaksiKeDalamRepository.java
+++ src/main/java/com/example/BankSampah/model/transaksiKeDalam/JdbcTransaksiKeDalamRepository.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -24,6 +25,14 @@ public class JdbcTransaksiKeDalamRepository implements TransaksiKeDalamRepositor
     public List<TransaksiKeDalam> findByUsername(String name) {
         String sql = "SELECT * FROM transaksiMasuk WHERE namapengguna = ?";
         return jdbcTemplate.query(sql, this::mapRowToSatuanKuantitas, name);
+    }
+
+    @Override
+    public List<Map<String, Object>> getLaporanSampah() {
+        String sql = "SELECT namasampah, satuankuantitas, SUM(jumlahsampah) AS total_kuantitas " +
+                     "FROM transaksimasuk " +
+                     "GROUP BY namasampah, satuankuantitas";
+        return jdbcTemplate.queryForList(sql);
     }
     
     @Override
